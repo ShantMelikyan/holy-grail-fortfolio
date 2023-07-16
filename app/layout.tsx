aa725c8d09a337e3c9eb4b3652dc6c81fa41621e
shantmelikyan <shantmelikyan@gmail.com>
2023-07-16 15:17:56 -0700

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "./providers";
-import img from "../public/android-chrome-192x192.png"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -78,6 +78,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} >
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
