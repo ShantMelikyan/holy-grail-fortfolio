07aed7f50549083a5a862e67beb1cb548876e5cf
shantmelikyan <shantmelikyan@gmail.com>
2023-07-17 13:33:23 -0700

    small changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import {FaLinkedin, FaGithub, FaRegEnvelope , FaInstagram} from "react-icons/fa"
 const Footer = () => {
   return (
     <footer className="w-full h-20 px-4 pb-4">
-      <div className="flex items-center justify-between w-full h-full max-w-3xl text-center m-auto p-6 rounded-full dark:bg-[#645d65a1] backdrop-filter backdrop-blur-md shadow-md">
+      <div className="flex items-center justify-between w-full h-full max-w-3xl text-center m-auto p-6 rounded-full dark:bg-[#645d65a1] backdrop-filter backdrop-blur-md shadow-md border border-[#645d6525]">
         <p className="text-sm">Design & Dev by Shant ©2023.</p>
         <div className="flex justify-end [&>*]:m-2">
                 <a
